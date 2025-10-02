f568c58b628bc0fec94f3d83570a7f535c73adcd
alex <shizofranky@gmail.com>
2025-10-02 21:25:21 +0300

    update

--- index.php
+++ index.php
@@ -2,15 +2,37 @@
 
 use daemon\Router;
 
+
+
+
 require_once "./models/router.php";
 
 $requestUri = $_SERVER['REQUEST_URI'];
+
+var_dump($requestUri);
 
 require_once './views/templates/header.php';
 require_once './views/' . Router::process($requestUri);
 require_once './views/templates/footer.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    ?>
+</body>
+
+</html>
 
 <a href="/bar">go bar</a>
 <a href="/foo">go home</a>
