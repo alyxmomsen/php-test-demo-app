15e340cdd64b45eb3729dd1cac1b6886191d1c1b
alex <shizofranky@gmail.com>
2025-10-04 14:10:09 +0300

    fixe bug wich send error to frontend

--- controllers/upload.javascript.php
+++ controllers/upload.javascript.php
@@ -1,21 +1,21 @@
 <?php
 // Отключить отображение ошибок на экране
-// ini_set('display_errors', 0);
+ini_set('display_errors', 0);
 // Включить запись ошибок в лог-файл
-// ini_set('log_errors', 1);
+ini_set('log_errors', 1);
 // Указать, куда писать ошибки (имя файла относительно текущего скрипта)
-// ini_set('error_log', 'php_upload_errors.log');
+ini_set('error_log', 'php_upload_errors.log');
 header('Content-Type: application/json');
 function foo()
 {
 
-    $var = $_FILES['media_file'];
+    // $var = $_FILES['media_file'];
     try {
         $var = $_FILES['media_file'];
         // var_dump($var);
-        // return $var;
+        return $var;
     } catch (Exception $e) {
-        // return $e;
+        return $e;
     }
 }
 // foo();
---
+++ readme.md
@@ -0,0 +1,3 @@
+## issues
+
+В корректном PHP коде, файла C:\mydata\portableProgs\OSPanel\home\test-project.local\controllers\upload.javascript.php, при 
